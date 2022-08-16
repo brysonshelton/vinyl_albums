46a3b10e5d52f3a55baa06d487b3c80a1f5ca2d1
Bryson Shelton <bryson1679@gmail.com>
2022-08-15 23:00:08 -0500

    adds a bunch of html files

--- vinyl_albums/collection/views.py
+++ vinyl_albums/collection/views.py
@@ -15,3 +15,54 @@ def equipment(request):
 
 def tsac(request):
     return render(request, "collection/the-sea-and-cake.html", {})
+
+def acb(request):
+    return render(request, "collection/as-cities-burn.html", {})
+
+def benkweller(request):
+    return render(request, "collection/ben-kweller.html", {})
+
+def dashboard(request):
+    return render(request, "collection/dashboard-confessional.html", {})
+
+def bowie(request):
+    return render(request, "collection/david-bowie.html", {})
+
+def fsf(request):
+    return render(request, "collection/further-seems-forever.html", {})
+
+def greenday(request):
+    return render(request, "collection/green-day.html", {})
+
+def guardians(request):
+    return render(request, "collection/guardians-of-the-galaxy.html", {})
+
+def jeremyenigk(request):
+    return render (request, "collection/jeremy-enigk.html", {})
+
+def julienbaker(request):
+    return render(request, "collection/julien-baker.html", {})
+
+def michaelbuble(request):
+    return render(request, "collection/michael-buble.html", {})
+
+def radiohead(request):
+    return render(request, "collection/radiohead.html", {})
+
+def samprekop(request):
+    return render(request, "collection/sam-prekop.html", {})
+
+def sunnyday(request):
+    return render(request, "collection/sunny-day-real-estate.html", {})
+
+def theanniversary(request):
+    return render(request, "collection/the-anniversary.html", {})
+
+def tortoise(request):
+    return render(request, "collection/tortoise.html", {})
+
+def vinceguaraldi(request):
+    return render(request, "collection/vince-guaraldi.html", {})
+
+def weezer(request):
+    return render(request, "collection/weezer.html", {})
